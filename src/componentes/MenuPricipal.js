2d8622b954956cb564f8f9940fa3d54b5724d794
sgoncalves13 <s.goncalves@uniandes.edu.co>
2024-06-11 19:39:55 -0500

    ret

--- src/componentes/MenuPricipal.js
+++ src/componentes/MenuPricipal.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import '../CSS/MenuPrincipal.css'
-import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useMsal } from '@azure/msal-react';
 
 function MenuPrincipal() {
 
@@ -12,8 +13,11 @@ function MenuPrincipal() {
 
      const [token, setToken] = useState(null);
 
+     const { instance } = useMsal();
+     const activeAccount = instance.getActiveAccount();
+
      const getIdToken = () => {
-        const tokenKey = 'de3c56ff-2d3f-4b4b-b334-d5e34e62e6b1.0a19c88b-7f2c-4593-908e-8b8a59a1e066-login.windows.net-idtoken-4b00258a-7f41-4167-a35e-fd4e679e5257-0a19c88b-7f2c-4593-908e-8b8a59a1e066---';
+        const tokenKey = `${activeAccount.idTokenClaims.oid}.${activeAccount.idTokenClaims.tid}-login.windows.net-idtoken-${activeAccount.idTokenClaims.aud}-${activeAccount.idTokenClaims.tid}---`;
         const idToken = JSON.parse(localStorage.getItem(tokenKey));
         localStorage.setItem('idToken', idToken.secret)
         console.log(idToken.secret)
